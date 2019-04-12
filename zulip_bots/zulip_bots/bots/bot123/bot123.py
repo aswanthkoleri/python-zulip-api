8d8241fabcf5916c8299f2ee1557210690af9888
aswanthkoleri <aswanth9495@gmail.com>
2019-04-12 17:23:13 +0530

    Add changes

--- zulip_bots/zulip_bots/bots/bot123/bot123.py
+++ zulip_bots/zulip_bots/bots/bot123/bot123.py
@@ -4,13 +4,20 @@ import os
 import sys
 import logging
 import configparser
-
+import pytz
 import requests
 
 from typing import Dict, Any, Tuple, Union
+from datetime import datetime, timezone
+# Reminder bot format : neo discussion on <subject> at <time> <Date>
+def utc_to_local(utc_dt):
+    local_dt= utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+    return local_tz.normalize(local_dt)
 
-# Reminder bot format : neo discussion on <subject> at <time> <Date> 
+def asLocalTimeString(utc_dt):
+    return utc_to_local(utc_dt).strftime('%Y-%m-%d %H:%M:%S.%f %Z%z')
 
+client = zulip.Client(config_file="~/zuliprc")
 
 class botHandler(object):
     def usage(self) -> str:
@@ -24,13 +31,28 @@ class botHandler(object):
             data=re.search(REGEX,messageContent,re.IGNORECASE)
             if data:
                bot_handler.send_reply(message,data.group(1)+" "+data.group(2)+" "+data.group(3))
+               print(message['message']['display_recipient'])
+               SCHEDULER = message['message']['display_recipient']
             #    convert UTC time to Local time and send the message to everyone in the stream
                 # UTC time
+                subject=data.group(1)
                 UTCtime = group(2)
                 UTCdate= group(3)
-                UTCtotal = UTCdate + " " + UTCtime
-                # Now we got the UTC time in a variable 
-                # We have to run a loop to send a PM to all the users in a stream.
+                UTC_string = UTCdate + " " + UTCtime
+                print(UTC_string)
+                # TODO: 1. Collect all the users in realm
+                # TODO: 2. Create the message for each user including the converted time
+                # TODO: 3. Send the message as PM to all the users in the Zulip realm
+                # 1. Collect all users in realm
+                all_users = client.get_members()
+                non_bot_users = [x for x in all_users['members'] if x['is_bot']==False ]
+                print(non_bot_users)
+                # 2. Create the message for each user including the converted time
+                for user in non_bot_users:
+                    #  Get the time stamp
+                    timezone = user['user']
+                    MESSAGE = "@**"+SCHEDULER+"** arranged a meeting about "+subject+"on "
+
+
                return
 handler_class=botHandler
-
