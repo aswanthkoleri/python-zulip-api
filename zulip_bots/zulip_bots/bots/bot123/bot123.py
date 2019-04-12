0ab23e0966817a663ca5041ccc171b98edd37f92
aswanthkoleri <aswanth9495@gmail.com>
2019-04-12 22:35:41 +0530

    Add changes to bot

--- zulip_bots/zulip_bots/bots/bot123/bot123.py
+++ zulip_bots/zulip_bots/bots/bot123/bot123.py
@@ -4,22 +4,35 @@ import os
 import sys
 import logging
 import configparser
-
+import zulip
 import requests
 
 from typing import Dict, Any, Tuple, Union
 
 # Reminder bot format : neo discussion on <subject> at <time> <Date> 
-
+client = zulip.Client(config_file="~/zuliprc")
 
 class botHandler(object):
     def usage(self) -> str:
         return "This is just a sample bot"
         # Discussion\s+on\s+(\w+)\s+at\s+(\w+)\s+(\w+)
     def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
-            print(message)
-            if message['content']=="help":
-               bot_handler.send_reply(message,"This is just a hlp message")
-               return
+            REGEX="Discussion\s+on\s+(.+)\s+at\s+(.+)\s+(.+)"
+            data=re.search(REGEX,message['content'],re.IGNORECASE)
+            if data:
+                # Have to get date time and subject in different variables
+                date=data.group(3)
+                time=data.group(2)
+                subject=data.group(1)
+                # Got the time date now get the timezone of the user and convert it to UTC
+                result = client.get_members()
+                print(result)
+                # Then take each user 
+                bot_handler.send_reply(message,"Date: "+date+"Time: "+time+"Subject: "+subject)
+
+            else:
+                bot_handler.send_reply(message,"Wrong format")
+
+                
 handler_class=botHandler
 
